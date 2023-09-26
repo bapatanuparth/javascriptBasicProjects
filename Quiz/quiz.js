8abb54cd13956b4e6ae68acc910869642ed6944e
bapatanuparth <parthanup@gmail.com>
2023-09-26 19:23:42 -0400

    quiz

---
+++ Quiz/quiz.js
@@ -0,0 +1,113 @@
+const data = [
+  {
+    id: 1,
+    question: "Which of these fish is actually a fish?",
+    answers: [
+      { answer: "swordfish", isCorrect: true },
+      { answer: "jellyfish", isCorrect: false },
+      { answer: "starfish", isCorrect: false },
+      { answer: "crayfish", isCorrect: false },
+    ],
+  },
+  {
+    id: 2,
+    question: "A flutter is a group of:",
+    answers: [
+      { answer: "bees", isCorrect: false },
+      { answer: "penguins", isCorrect: false },
+      { answer: "butterflies", isCorrect: true },
+      { answer: "camels", isCorrect: false },
+    ],
+  },
+  {
+    id: 1,
+    question: "A group of which animals is referred to as a wake?",
+    answers: [
+      { answer: "bats", isCorrect: false },
+      { answer: "vultures", isCorrect: true },
+      { answer: "ants", isCorrect: false },
+    ],
+  },
+];
+
+const gameScreen = document.querySelector(".game");
+const resultScreen = document.querySelector(".result");
+const question = document.querySelector(".question");
+const answersContainer = document.querySelector(".answers");
+const submit = document.querySelector(".submit");
+const play = document.querySelector(".play");
+
+let qIndex = 0;
+let correctAns = 0;
+let wrongAns = 0;
+let total = 0;
+let selectedAns;
+
+const playAgain = () => {
+  qIndex = 0;
+  correctAns = 0;
+  wrongAns = 0;
+  total = 0;
+  selectedAns;
+  showQuestion(qIndex);
+};
+
+play.addEventListener("click", () => {
+  resultScreen.style.display = "none";
+  gameScreen.style.display = "block";
+  playAgain();
+});
+
+const showResult = () => {
+  resultScreen.style.display = "block";
+  gameScreen.style.display = "none";
+
+  resultScreen.querySelector(
+    ".correct"
+  ).textContent = `Correct Answers:  ${correctAns}`;
+  resultScreen.querySelector(
+    ".wrong"
+  ).textContent = `Wrong Answers:  ${wrongAns}`;
+  resultScreen.querySelector(".score").textContent = `Score:  ${
+    (correctAns - wrongAns) * 10
+  }`;
+};
+
+const showQuestion = (qNumber) => {
+  if (qIndex === data.length) return showResult();
+  selectedAns = null;
+  question.textContent = data[qNumber].question;
+  answersContainer.innerHTML = data[qNumber].answers
+    .map(
+      (item, index) =>
+        `<div class="answer">
+              <input type="radio" name="answer" id="${index}" value="${item.isCorrect}" />
+              <label for="${index}">${item.answer}</label>
+            </div>`
+    )
+    .join("");
+  selectAnswer();
+};
+
+const selectAnswer = () => {
+  answersContainer.querySelectorAll("input").forEach((el) => {
+    el.addEventListener("click", (e) => {
+      selectedAns = e.target.value;
+    });
+  });
+};
+
+const submitAnswer = () => {
+  submit.addEventListener("click", () => {
+    if (selectedAns !== null) {
+      selectedAns === "true" ? correctAns++ : wrongAns++;
+      qIndex++;
+      showQuestion(qIndex);
+    } else {
+      alert("select an answer! ");
+    }
+  });
+};
+
+showQuestion(qIndex);
+submitAnswer();
